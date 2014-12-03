25e57660014638e1319011bfd17989bff0eb4a70
Zack Middleton <zack@cloemail.com>
2014-12-03 00:22:02 -0600

    Make TA voice chat head stick to left side in widescreen
    The HUD is drawn centered, but voice chat head should always be at left side
    so that console notify/chat text doesn't draw over it.
    
    Commit imported from mint-arena.

--- code/ui/ui_shared.c
+++ code/ui/ui_shared.c
@@ -4195,6 +4195,15 @@ void Menu_SetFeederSelection(menuDef_t *menu, int feeder, int index, const char 
 	}
 }
 
+void Menu_SetScreenPlacement(menuDef_t *menu, screenPlacement_e hpos, screenPlacement_e vpos ) {
+	if ( !menu )
+		return;
+
+	menu->forceScreenPlacement = qtrue;
+	menu->screenHPos = hpos;
+	menu->screenVPos = vpos;
+}
+
 qboolean Menus_AnyFullScreenVisible(void) {
   int i;
   for (i = 0; i < menuCount; i++) {
@@ -4327,6 +4336,10 @@ void Menu_Paint(menuDef_t *menu, qboolean forcePaint) {
 		menu->window.flags |= WINDOW_FORCED;
 	}
 
+	if (menu->forceScreenPlacement) {
+		CG_SetScreenPlacement( menu->screenHPos, menu->screenVPos );
+	}
+
 	// draw the background if necessary
 	if (menu->fullScreen) {
 		// implies a background shader
@@ -4349,6 +4362,10 @@ void Menu_Paint(menuDef_t *menu, qboolean forcePaint) {
 		color[0] = color[2] = color[3] = 1;
 		color[1] = 0;
 		DC->drawRect(menu->window.rect.x, menu->window.rect.y, menu->window.rect.w, menu->window.rect.h, 1, color);
+	}
+
+	if (menu->forceScreenPlacement) {
+		CG_PopScreenPlacement();
 	}
 }
 
